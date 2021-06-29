b7b63eed5c2657a69c5b25ae2b970219d3f60624
Thad <thadisfree@gmail.com>
2021-06-28 21:29:57 -0500

    commented out some code

--- AdditionalGitPractice3/Program.cs
+++ AdditionalGitPractice3/Program.cs
@@ -7,6 +7,7 @@ namespace AdditionalGitPractice3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //made some changes here//
         }
     }
 }
